d6a6d298d1332d58348c19b0739c632f1b861864
misaels-r <msr.elestudio@gmail.com>
2024-06-12 11:30:02 +0200

    CARTESIUS_WEBBUILD

---
+++ StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,115 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_CARTESIUS_SOUNDS_L_ENTROPIE = 4117692096U;
+        static const AkUniqueID PLAY_CARTESIUS_SOUNDS_LA_ENACCI_N = 1787033799U;
+        static const AkUniqueID PLAY_CARTESIUS_SOUNDS_LA_TRAICI_N = 374810558U;
+        static const AkUniqueID PLAY_CARTESIUS_SOUNDS_LES_LOGARITMES = 541470021U;
+        static const AkUniqueID PLAY_PAD_I = 540071185U;
+        static const AkUniqueID PLAY_PAD_II = 3185535402U;
+        static const AkUniqueID PLAY_PAD_III = 2427672311U;
+        static const AkUniqueID PLAY_PAD_IV = 3185535413U;
+        static const AkUniqueID PLAY_STEPS = 2600469463U;
+        static const AkUniqueID PLAY_STEPS2 = 3624274759U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace I
+        {
+            static const AkUniqueID GROUP = 84696438U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID OFF_I = 1620381984U;
+                static const AkUniqueID ON_I = 2976405622U;
+            } // namespace STATE
+        } // namespace I
+
+        namespace II
+        {
+            static const AkUniqueID GROUP = 1752637611U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID OFF_II = 715781385U;
+                static const AkUniqueID ON_II = 3175301547U;
+            } // namespace STATE
+        } // namespace II
+
+        namespace III
+        {
+            static const AkUniqueID GROUP = 512257368U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID OFF_III = 848084290U;
+                static const AkUniqueID ON_III = 2615173208U;
+            } // namespace STATE
+        } // namespace III
+
+        namespace IV
+        {
+            static const AkUniqueID GROUP = 1752637620U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID OFF_IV = 715781398U;
+                static const AkUniqueID ON_IV = 3175301556U;
+            } // namespace STATE
+        } // namespace IV
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID SPEED = 640949982U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID BOXES = 2016903052U;
+        static const AkUniqueID CHARACTER = 436743010U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID PADS = 1778030307U;
+        static const AkUniqueID REVERB = 348963605U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID REVERB1 = 3545700926U;
+        static const AkUniqueID REVERB2 = 3545700925U;
+        static const AkUniqueID REVERB3 = 3545700924U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
